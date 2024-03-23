3df2b4ff5c59d1446c7d64a41fd3a5ed3f869bd8
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-03-24 00:47:37 +0530

    Added Package Information

--- witransfer/src/main.rs
+++ witransfer/src/main.rs
@@ -2,6 +2,10 @@ use clap::Parser;
 
 /// Welcome to `WiTransfer`
 #[derive(Parser)]
+#[command(name = "WiTransfer")]
+#[command(about = "A command-line tool for transferring files over Wi-Fi.")]
+#[command(version, long_about = None)]
+#[command(propagate_version  = true)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
