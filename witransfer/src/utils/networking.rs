08c782414471182e598e0dc0e7720dd6f7596721
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-03-26 21:08:28 +0530

    Added basic message processing.
    + Optimized imports
    + Using multi-threading
    + Communication between multiple threads

--- witransfer/src/utils/networking.rs
+++ witransfer/src/utils/networking.rs
@@ -1,16 +1,19 @@
+use local_ip_address;
 use log::info;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::net::{IpAddr, SocketAddr, UdpSocket};
-use std::sync::Arc;
-use std::thread;
-use std::thread::sleep;
+use std::sync::{
+    mpsc::{self, Receiver, Sender},
+    Arc, Mutex,
+};
+use std::thread::{self, sleep};
 use std::time::Duration;
 use whoami;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Message<'a> {
-    identifier: &'a str,
+struct Message {
+    identifier: String,
     device_info: DeviceInfo,
     ip_addr: IpAddr,
     max_threads: usize,
@@ -25,7 +28,7 @@ struct DeviceInfo {
     distro: String,
 }
 
-impl<'a> Message<'a> {
+impl Message {
     fn as_bytes(&self) -> Vec<u8> {
         serde_json::to_vec(self).expect("Failed to serialize message")
     }
@@ -49,7 +52,7 @@ impl<'a> Message<'a> {
 /// The function will panic if it encounters error with Socket or sending visibility message.
 pub fn discover(port: u16) {
     let message = Message {
-        identifier: "WiTransfer",
+        identifier: "WiTransfer".to_string(),
         device_info: DeviceInfo {
             real_name: whoami::realname(),
             user_name: whoami::username(),
@@ -67,18 +70,27 @@ pub fn discover(port: u16) {
         .set_broadcast(true)
         .expect("Unable to set broadcast option.");
     let socket_clone = Arc::clone(&socket);
+    let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || receive_visibility_message(socket_clone));
+    thread::spawn(move || receive_visibility_message(socket_clone, tx));
 
-    loop {
+    let devices: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+    let devices_clone = Arc::clone(&devices);
+    thread::spawn(move || filter_response(devices_clone, rx));
+
+    thread::spawn(move || loop {
         send_visibility_message(&socket, port, &message);
         sleep(Duration::from_secs(2));
-    }
+    });
+
+    // TODO: Make user select which device they want to connect with.
 }
 
+// Sends visibility message on the broadcast
 fn send_visibility_message(socket: &Arc<UdpSocket>, port: u16, message: &Message) {
     let broadcast_addr = SocketAddr::new(IpAddr::V4("255.255.255.255".parse().unwrap()), port);
 
+    // TODO: Optimize and reduce socket message if large
     let send_buf = socket.send_to(&message.as_bytes(), broadcast_addr);
 
     match send_buf {
@@ -89,7 +101,8 @@ fn send_visibility_message(socket: &Arc<UdpSocket>, port: u16, message: &Message
     }
 }
 
-fn receive_visibility_message(socket: Arc<UdpSocket>) {
+// Receive visibility message and send message to another thread for processing.
+fn receive_visibility_message(socket: Arc<UdpSocket>, tx: Sender<Message>) {
     info!("Awaiting for responses");
     socket
         .set_read_timeout(Some(Duration::from_secs(50)))
@@ -104,7 +117,7 @@ fn receive_visibility_message(socket: Arc<UdpSocket>) {
                 // println!("Received {} bytes from {}: {:?}", n, addr, data);
                 let message: Message =
                     serde_json::from_slice(data).expect("Unable to read the message.");
-                println!("{:#?}", message);
+                tx.send(message).unwrap();
                 // Clear the buffer
                 buf = [0; 1024];
             }
@@ -112,6 +125,26 @@ fn receive_visibility_message(socket: Arc<UdpSocket>) {
                 eprintln!("Error receiving data: {}", e);
                 break; // Exit the loop on error
             }
+        }
+    }
+}
+
+// Process/filter the message received from other thread
+fn filter_response(devices: Arc<Mutex<Vec<String>>>, rx: Receiver<Message>) {
+    loop {
+        match rx.recv() {
+            Ok(message) => {
+                if message.identifier == "WiTransfer".to_string()
+                && message.ip_addr != local_ip_address::local_ip().unwrap()
+                {
+                    // TODO: Logic for what to show user.
+                    devices.lock().unwrap().push(format!(
+                        "{} - {}",
+                        message.device_info.real_name, message.device_info.device_name
+                    ));
+                }
+            }
+            _ => continue,
         }
     }
 }
